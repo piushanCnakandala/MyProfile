ae9582d06aae754da78560760bfb79a2cc6aadf6
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-01-27 00:22:43 +0530

    home click nav usin js

--- myproject/SPA/assets/js/script.js
+++ myproject/SPA/assets/js/script.js
@@ -1,4 +1,11 @@
 
-  /*hide order page and item page*/
+  /*hide order part  and item part*/
 document.getElementById("ItemPage").style.setProperty("Display","none","important");
 document.getElementById("Order Page").style.setProperty("Display","none","important");
+
+ /*click event*/
+document.getElementById("click-Home").addEventListener("click",function (){
+    document.getElementById("HomePage").style.setProperty("Display","flex","important");
+    document.getElementById("ItemPage").style.setProperty("Display","none","important");
+    document.getElementById("Order Page").style.setProperty("Display","none","important");
+});
