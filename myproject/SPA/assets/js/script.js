c30e2775a770f602f1af9162b1cf9923a845baa6
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-01-27 09:33:19 +0530

    SPA is completed

--- myproject/SPA/assets/js/script.js
+++ myproject/SPA/assets/js/script.js
@@ -3,9 +3,21 @@
 document.getElementById("ItemPage").style.setProperty("Display","none","important");
 document.getElementById("Order Page").style.setProperty("Display","none","important");
 
- /*click event*/
+ /*click event navbar*/
 document.getElementById("click-Home").addEventListener("click",function (){
-    document.getElementById("HomePage").style.setProperty("Display","flex","important");
+    document.getElementById("HomePage").style.setProperty("Display","block","important");
     document.getElementById("ItemPage").style.setProperty("Display","none","important");
     document.getElementById("Order Page").style.setProperty("Display","none","important");
 });
+
+  document.getElementById("click-Item").addEventListener("click",function (){
+      document.getElementById("ItemPage").style.setProperty("Display","block","important");
+      document.getElementById("HomePage").style.setProperty("Display","none","important");
+      document.getElementById("Order Page").style.setProperty("Display","none","important");
+  });
+
+  document.getElementById("click-Order").addEventListener("click",function (){
+      document.getElementById("Order Page").style.setProperty("Display","block","important");
+      document.getElementById("HomePage").style.setProperty("Display","none","important");
+      document.getElementById("ItemPage").style.setProperty("Display","none","important");
+  });
