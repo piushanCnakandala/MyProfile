ca6772082abade5a8ba04ffacb16a0964a28d373
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-02-15 11:58:35 +0530

    input item data content update

--- myproject/SPA/assets/jQuery/jQuery.js
+++ myproject/SPA/assets/jQuery/jQuery.js
@@ -20,7 +20,7 @@ $("#addCust").click(function (){
 
         $("#customerTable>tr").off("click"); //of click events
 
-         let customerName= $(this).children(":eq(0)").text();
+        let customerName= $(this).children(":eq(0)").text();
         let customerAge= $(this).children(":eq(1)").text();
         let customerTp= $(this).children(":eq(2)").text();
         let customerSalary= $(this).children(":eq(3)").text();
@@ -54,8 +54,56 @@ $("#inputTp").keydown(function (event){
     if (event.key=="Enter"){
         $("#inputSalary").focus();
     }
-})
+});
 
 
-/*item*/
+                      /*item*/
+             //input data to text fields
+             $("#addItem").click(function (){
+                 let itemName =$("#inputItemName").val();
+                 let itemPrice =$("#inputPrice").val();
+                 let itemQuantity =$("#inputQuantity").val();
+                 let price =$("#inputData").val();
+//input data to table
+                 let raw=`<tr><td>${itemName}</td><td>${itemPrice}</td><td>${itemQuantity}</td><td>${price}</td></tr>`
+                 $("#itemTable").append(raw);
 
+//return data to the text fields
+                 $("#itemTable>tr").click(function(){
+
+                     $("#itemTable>tr").off("click"); //of click events
+
+                     let itemName= $(this).children(":eq(0)").text();
+                     let itemPrice= $(this).children(":eq(1)").text();
+                     let itemQuantity= $(this).children(":eq(2)").text();
+                     let price= $(this).children(":eq(3)").text();
+
+
+                     //set vales for the input fields
+                     $("#inputItemName").val(itemName);
+                     $("#inputPrice").val(itemPrice);
+                     $("#inputQuantity").val(itemQuantity);
+                     $("#inputData").val(price);
+
+                 });
+
+
+             });
+             //text fields focusing
+             $("#inputItemName").keydown(function (event){
+                 if (event.key=="Enter"){
+                     $("#inputPrice").focus();
+                 }
+             });
+
+             $("#inputPrice").keydown(function (event){
+                 if (event.key=="Enter"){
+                     $("#inputQuantity").focus();
+                 }
+             });
+
+             $("#inputQuantity").keydown(function (event){
+                 if (event.key=="Enter"){
+                     $("#inputData").focus();
+                 }
+             });
